5c1a9afbad9340b53b4802e95019426020d8ab20
mpietragalla <mpietragalla@gmail.com>
2017-02-07 10:43:02 -0200

    versao2

--- IKsolver/sketch.js
+++ IKsolver/sketch.js
@@ -6,17 +6,22 @@ arm  = new Arm(createVector(width/2,height/2), 60,60);
 
 function draw() {
 background(200);
-if(mouseIsPressed && dist(mouseX, mouseY, arm.fist.x, arm.fist.y)<arm.jointRadius/2){
-arm.isDragging = true;
+if(mouseIsPressed){
+
+	if(dist(mouseX, mouseY, arm.fist.x, arm.fist.y)<arm.jointRadius/2 && !arm.draggingPivot) arm.draggingFist = true;
+
+	if(dist(mouseX, mouseY, arm.pivot.x, arm.pivot.y)<arm.jointRadius/4 && !arm.draggingFist) arm.draggingPivot = true;
+
 }else if(!mouseIsPressed){
-	arm.isDragging = false;
+	arm.draggingFist = false;
+	arm.draggingPivot = false;
 	
 }
 
 arm.render();
   
   
- if(arm.isDragging){
+ if(arm.draggingFist || arm.draggingPivot){
 	arm.IKSolver(mouseX, mouseY);
  }
 }
@@ -27,48 +32,94 @@ function Arm(pivot, len1, len2){
 this.pivot = pivot;
 this.foreArmLen = len1;
 this.armLen = len2;
-this.foreArmAngle = radians(0);
-this.armAngle = radians(0);
+this.foreArmAngle = radians(10);
+this.armAngle = radians(10);
 this.elbow = createVector(this.pivot.x+this.armLen*cos(this.armAngle), this.pivot.y +this.armLen*sin(this.armAngle));
 this.fist = createVector(this.elbow.x+this.foreArmLen*cos(this.armAngle+this.foreArmAngle), this.elbow.y+this.foreArmLen*sin(this.armAngle+this.foreArmAngle));
 this.jointRadius = 30;
 this.midPoint = createVector(0,0);
 
-this.isDragging = false;
+this.draggingPivot = false;
+this.draggingFist = false;
 
 this.IKSolver = function(mX, mY){
-	var dif =  p5.Vector.sub(createVector(mX,mY), this.pivot);
-	
-	var dis = constrain(dif.mag(),-2*this.armLen, 2*this.armLen);
-	
-	if(mX<width/2) 
-		dis*=-1;
-	var midDis = dis/2;
-	
-	this.midPoint = p5.Vector.add(this.pivot, dif.setMag(midDis));
-	
-	this.armAngle = atan(dif.y/dif.x) -acos(midDis/this.armLen);
-	
-	
-	
-	this.elbow = createVector(this.pivot.x+this.armLen*cos(this.armAngle), this.pivot.y +this.armLen*sin(this.armAngle));
-	
-	
-	if(dif.mag()> 2*this.armLen) 
-		dif.setMag( 2*this.armLen);
+	if(this.draggingFist){
+		var dif =  p5.Vector.sub(createVector(mX,mY), this.pivot);
+		console.log(dif);
 		
-	dif.mult(2);
+		var dis = constrain(dif.mag(),-2*this.armLen, 2*this.armLen);
+		
+		if(mX<this.pivot.x) 
+			dis*=-1;
+		var midDis = dis/2;
+		
+		
+		
+		this.midPoint = p5.Vector.add(this.pivot, dif.setMag(midDis));
+		
+		var mult = 1;
+		if(mX<this.pivot.x) 
+			mult*=-1;
+		this.armAngle = atan(dif.y/(dif.x+0.0001)) -acos(midDis/this.armLen)*mult;
+		console.log(this.armAngle+" = "+atan(dif.y/(dif.x+0.0001))+" - "+acos(midDis/this.armLen) );
+		
+		
+		
+		
+		this.elbow = createVector(this.pivot.x+(this.armLen*cos(this.armAngle)), this.pivot.y +(this.armLen*sin(this.armAngle)));
+		
+		
+		if(dif.mag()> 2*this.armLen) 
+			dif.setMag( 2*this.armLen);
+			
+		dif.mult(2);
+		
+		if(mX<this.pivot.x) 
+			dif.mult(-1);
+		
+		this.fist = p5.Vector.add(this.pivot, dif);
+	}else if(this.draggingPivot){
+		var dif =  p5.Vector.sub(createVector(mX,mY), this.fist);
+		console.log(dif);
+		
+		var dis = constrain(dif.mag(),-2*this.armLen, 2*this.armLen);
+		
+		if(mX<this.fist.x) 
+			dis*=-1;
+		var midDis = dis/2;
+		
+		
+		
+		this.midPoint = p5.Vector.add(this.fist, dif.setMag(midDis));
+		
+		var mult = 1;
+		if(mX>this.fist.x) 
+			mult*=-1;
+		this.armAngle = atan(dif.y/(dif.x+0.0001)) -acos(midDis/this.armLen)*mult;
+		console.log(this.armAngle+" = "+atan(dif.y/(dif.x+0.0001))+" - "+acos(midDis/this.armLen) );
+		
+		
+		
+		
+		this.elbow = createVector(this.fist.x+(this.armLen*cos(this.armAngle)), this.fist.y +(this.armLen*sin(this.armAngle)));
+		
+		
+		if(dif.mag()> 2*this.armLen) 
+			dif.setMag( 2*this.armLen);
+			
+		dif.mult(2);
+		
+		if(mX<this.fist.x) 
+			dif.mult(-1);
+		
+		this.pivot = p5.Vector.add(this.fist, dif);
 	
-	if(mX<width/2) 
-		dif.mult(-1);
-	
-	this.fist = p5.Vector.add(this.pivot, dif);
+	}
 	
 	
 	
 
 }
-
 this.render = function(){
 	push();
 
@@ -80,10 +131,12 @@ this.render = function(){
 		
 		fill(255);
 		strokeWeight(2);
-		ellipse(this.pivot.x,this.pivot.y,this.jointRadius/2,this.jointRadius/2);
+		
 		ellipse(this.elbow.x,this.elbow.y,this.jointRadius/2,this.jointRadius/2);
-		if(this.isDragging) fill(255,0,0);
+		if(this.draggingFist) fill(255,0,0); else fill(200);
 		ellipse(this.fist.x,this.fist.y,this.jointRadius,this.jointRadius);
+		if(this.draggingPivot) fill(255,0,0); else fill(200);
+		ellipse(this.pivot.x,this.pivot.y,this.jointRadius/2,this.jointRadius/2);
 		
 	pop();
 
